76d741b6690b28d647763b88a0df357f5333c213
Mike Moran <mike@houseofmoran.com>
2022-09-03 21:09:56 +0100

    only print info for `Aranet4` peripherals

--- probe/src/main.rs
+++ probe/src/main.rs
@@ -34,46 +34,40 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     .unwrap()
                     .local_name
                     .unwrap_or(String::from("(peripheral name unknown)"));
-                println!(
-                    "Peripheral {:?} is connected: {:?}",
-                    local_name, is_connected
-                );
                 if !is_connected && local_name.contains("Aranet4"){
                     println!("Connecting to peripheral {:?}...", &local_name);
                     if let Err(err) = peripheral.connect().await {
                         eprintln!("Error connecting to peripheral, skipping: {}", err);
                         continue;
                     }
-                }
-                let is_connected = peripheral.is_connected().await?;
-                println!(
-                    "Now connected ({:?}) to peripheral {:?}...",
-                    is_connected, &local_name
-                );
-                peripheral.discover_services().await?;
-                println!("Discover peripheral {:?} services...", &local_name);
-                for service in peripheral.services() {
+                    let is_connected = peripheral.is_connected().await?;
                     println!(
-                        "Service UUID {}, primary: {}",
-                        service.uuid, service.primary
+                        "Now connected ({:?}) to peripheral {:?}...",
+                        is_connected, &local_name
                     );
-                    for characteristic in service.characteristics {
-                        println!("  {:?}", characteristic);
-                        if characteristic.properties.contains(CharPropFlags::READ) {
-                            let response = peripheral.read(&characteristic).await.expect("failed read");
-                            println!("response: {:?}", response);
-                            if format!("{:?}", characteristic.uuid).contains("f0cd1503-95da-4f4b-9ac8-aa55d312af0c") {
-                                let co2 = u16::from_le_bytes([ response[0], response[1] ]);
-                                let temp = u16::from_le_bytes([ response[2], response[3] ]) as f32 / 20.0;
-                                let pressure = u16::from_le_bytes([ response[4], response[5] ]) as f32 / 10.0;
-                                let humidity = response[6];
-                                let battery = response[7];
-                                println!("ARANET_CO2_MEASUREMENT_CHARACTERISTIC_UUID = {}, {}, {}, {}, {}", co2, temp, pressure, humidity, battery);
+                    peripheral.discover_services().await?;
+                    println!("Discover peripheral {:?} services...", &local_name);
+                    for service in peripheral.services() {
+                        println!(
+                            "Service UUID {}, primary: {}",
+                            service.uuid, service.primary
+                        );
+                        for characteristic in service.characteristics {
+                            println!("  {:?}", characteristic);
+                            if characteristic.properties.contains(CharPropFlags::READ) {
+                                let response = peripheral.read(&characteristic).await.expect("failed read");
+                                println!("response: {:?}", response);
+                                if format!("{:?}", characteristic.uuid).contains("f0cd1503-95da-4f4b-9ac8-aa55d312af0c") {
+                                    let co2 = u16::from_le_bytes([ response[0], response[1] ]);
+                                    let temp = u16::from_le_bytes([ response[2], response[3] ]) as f32 / 20.0;
+                                    let pressure = u16::from_le_bytes([ response[4], response[5] ]) as f32 / 10.0;
+                                    let humidity = response[6];
+                                    let battery = response[7];
+                                    println!("ARANET_CO2_MEASUREMENT_CHARACTERISTIC_UUID = {}, {}, {}, {}, {}", co2, temp, pressure, humidity, battery);
+                                }
                             }
                         }
                     }
-                }
-                if is_connected {
                     println!("Disconnecting from peripheral {:?}...", &local_name);
                     peripheral
                         .disconnect()
