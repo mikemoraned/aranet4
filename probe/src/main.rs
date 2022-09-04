ccd0fa01b2ffdbf5a7a1d867ca2a8d1270f9d81f
Mike Moran <mike@houseofmoran.com>
2022-09-04 18:19:04 +0100

    - limit search to known service uuid

--- probe/src/main.rs
+++ probe/src/main.rs
@@ -24,6 +24,9 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let aranet_local_name_prefix = "Aranet4";
+    let aranet_service_uuid : Uuid 
+        = Uuid::parse_str("f0cd1400-95da-4f4b-9ac8-aa55d312af0c")?;
     let aranet_co2_measurement_characteristic_uuid : Uuid 
         = Uuid::parse_str("f0cd1503-95da-4f4b-9ac8-aa55d312af0c")?;
 
@@ -54,7 +57,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         if peripherals.is_empty() {
             error!("->>> BLE peripheral devices were not found, sorry. Exiting...");
         } else {
-            // All peripheral devices in range
+            info!("Finding all peripheral devices in range");
             for peripheral in peripherals.iter() {
                 let properties = peripheral.properties().await?;
                 let is_connected = peripheral.is_connected().await?;
@@ -62,7 +65,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     .unwrap()
                     .local_name
                     .unwrap_or(String::from("(peripheral name unknown)"));
-                if !is_connected && local_name.contains("Aranet4"){
+                if !is_connected && local_name.contains(aranet_local_name_prefix){
                     info!("Connecting to peripheral {:?}...", &local_name);
                     if let Err(err) = peripheral.connect().await {
                         error!("Error connecting to peripheral, skipping: {}", err);
@@ -76,23 +79,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     peripheral.discover_services().await?;
                     info!("Discover peripheral {:?} services...", &local_name);
                     for service in peripheral.services() {
-                        info!(
-                            "Service UUID {}, primary: {}",
-                            service.uuid, service.primary
-                        );
-                        for characteristic in service.characteristics {
-                            if characteristic.uuid == aranet_co2_measurement_characteristic_uuid
-                                && characteristic.properties.contains(CharPropFlags::READ) {
-                                let response = peripheral.read(&characteristic).await.expect("failed read");
-                                debug!("response: {:?}", response);
-                                let sample = Sample {
-                                    co2 : u16::from_le_bytes([ response[0], response[1] ]),
-                                    temp : u16::from_le_bytes([ response[2], response[3] ]) as f32 / 20.0,
-                                    pressure : u16::from_le_bytes([ response[4], response[5] ]) as f32 / 10.0,
-                                    humidity : response[6],
-                                    battery : response[7],
-                                };
-                                info!("{}: ARANET_CO2_MEASUREMENT_CHARACTERISTIC_UUID: {:?}", &local_name, sample);
+                        if service.uuid == aranet_service_uuid {
+                            info!(
+                                "Service UUID {}, primary: {}",
+                                service.uuid, service.primary
+                            );
+                            for characteristic in service.characteristics {
+                                if characteristic.uuid == aranet_co2_measurement_characteristic_uuid
+                                    && characteristic.properties.contains(CharPropFlags::READ) {
+                                    let response = peripheral.read(&characteristic).await.expect("failed read");
+                                    debug!("response: {:?}", response);
+                                    let sample = Sample {
+                                        co2 : u16::from_le_bytes([ response[0], response[1] ]),
+                                        temp : u16::from_le_bytes([ response[2], response[3] ]) as f32 / 20.0,
+                                        pressure : u16::from_le_bytes([ response[4], response[5] ]) as f32 / 10.0,
+                                        humidity : response[6],
+                                        battery : response[7],
+                                    };
+                                    info!("{}: ARANET_CO2_MEASUREMENT_CHARACTERISTIC_UUID: {:?}", &local_name, sample);
+                                }
                             }
                         }
                     }
