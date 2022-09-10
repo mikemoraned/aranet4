32e4dc7e3577e36dc8b8d6e95924b8f61be22c85
Mike Moran <mike@houseofmoran.com>
2022-09-10 20:26:41 +0100

    found device by service uuid

--- probe/src/main.rs
+++ probe/src/main.rs
@@ -5,8 +5,11 @@ use env_logger::Env;
 use tokio::time;
 use uuid::Uuid;
 
-use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter, CharPropFlags};
-use btleplug::platform::Manager;
+// use btleplug::api::{Central, Manager as _, Peripheral, ScanFilter, CharPropFlags};
+// use btleplug::platform::Manager;
+
+use bluest::Adapter;
+use futures_util::StreamExt;
 
 mod cli;
 mod sample;
@@ -32,71 +35,91 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     info!("Running {} v{}, with {:?}", NAME, VERSION, cli);
 
-    let manager = Manager::new().await?;
-    let adapter_list = manager.adapters().await?;
-    if adapter_list.is_empty() {
-        error!("No Bluetooth adapters found");
+    let adapter = Adapter::default().await.ok_or("Bluetooth adapter not found")?;
+    adapter.wait_available().await?;
+
+    info!("starting scan");
+    let services = vec![aranet_service_uuid];
+    let mut scan = adapter.scan(&services).await?;
+    info!("scan started");
+
+    while let Some(discovered_device) = scan.next().await {
+        info!(
+            "{}{}: {:?}",
+            discovered_device.device.name().as_deref().unwrap_or("(unknown)"),
+            discovered_device
+                .rssi
+                .map(|x| format!(" ({}dBm)", x))
+                .unwrap_or_default(),
+            discovered_device.adv_data.services
+        );
     }
 
-    for adapter in adapter_list.iter() {
-        info!("Starting {:?} scan on {}...", scan_length_duration, adapter.adapter_info().await?);
-        adapter
-            .start_scan(ScanFilter::default())
-            .await
-            .expect("Can't scan BLE adapter for connected devices...");
-        time::sleep(scan_length_duration).await;
-        let peripherals = adapter.peripherals().await?;
-        if peripherals.is_empty() {
-            error!("->>> BLE peripheral devices were not found, sorry. Exiting...");
-        } else {
-            info!("Finding all peripheral devices in range");
-            for peripheral in peripherals.iter() {
-                let properties = peripheral.properties().await?;
-                let is_connected = peripheral.is_connected().await?;
-                let local_name = properties
-                    .unwrap()
-                    .local_name
-                    .unwrap_or(String::from("(peripheral name unknown)"));
-                if !is_connected && local_name.contains(aranet_local_name_prefix){
-                    info!("Connecting to peripheral {:?}...", &local_name);
-                    if let Err(err) = peripheral.connect().await {
-                        error!("Error connecting to peripheral, skipping: {}", err);
-                        continue;
-                    }
-                    let is_connected = peripheral.is_connected().await?;
-                    info!(
-                        "Now connected ({:?}) to peripheral {:?}...",
-                        is_connected, &local_name
-                    );
-                    peripheral.discover_services().await?;
-                    info!("Discover peripheral {:?} services...", &local_name);
-                    for service in peripheral.services() {
-                        if service.uuid == aranet_service_uuid {
-                            info!(
-                                "Service UUID {}, primary: {}",
-                                service.uuid, service.primary
-                            );
-                            for characteristic in service.characteristics {
-                                if characteristic.uuid == aranet_co2_measurement_characteristic_uuid
-                                    && characteristic.properties.contains(CharPropFlags::READ) {
-                                    let response = peripheral.read(&characteristic).await.expect("failed read");
-                                    debug!("response: {:?}", response);
-                                    let sample = sample::Sample::try_from(&response)?;
-                                    info!("{}: ARANET_CO2_MEASUREMENT_CHARACTERISTIC_UUID: {:?}", &local_name, sample);
-                                }
-                            }
-                        }
-                    }
-                    info!("Disconnecting from peripheral {:?}...", &local_name);
-                    peripheral
-                        .disconnect()
-                        .await
-                        .expect("Error disconnecting from BLE peripheral");
-                }
-            }
-        }
+    // let manager = Manager::new().await?;
+    // let adapter_list = manager.adapters().await?;
+    // if adapter_list.is_empty() {
+    //     error!("No Bluetooth adapters found");
+    // }
+
+    // for adapter in adapter_list.iter() {
+    //     info!("Starting {:?} scan on {}...", scan_length_duration, adapter.adapter_info().await?);
+    //     adapter
+    //         .start_scan(ScanFilter::default())
+    //         .await
+    //         .expect("Can't scan BLE adapter for connected devices...");
+    //     time::sleep(scan_length_duration).await;
+    //     let peripherals = adapter.peripherals().await?;
+    //     if peripherals.is_empty() {
+    //         error!("->>> BLE peripheral devices were not found, sorry. Exiting...");
+    //     } else {
+    //         info!("Finding all peripheral devices in range");
+    //         for peripheral in peripherals.iter() {
+    //             let properties = peripheral.properties().await?;
+    //             let is_connected = peripheral.is_connected().await?;
+    //             let local_name = properties
+    //                 .unwrap()
+    //                 .local_name
+    //                 .unwrap_or(String::from("(peripheral name unknown)"));
+    //             if !is_connected && local_name.contains(aranet_local_name_prefix){
+    //                 info!("Connecting to peripheral {:?}...", &local_name);
+    //                 if let Err(err) = peripheral.connect().await {
+    //                     error!("Error connecting to peripheral, skipping: {}", err);
+    //                     continue;
+    //                 }
+    //                 let is_connected = peripheral.is_connected().await?;
+    //                 info!(
+    //                     "Now connected ({:?}) to peripheral {:?}...",
+    //                     is_connected, &local_name
+    //                 );
+    //                 peripheral.discover_services().await?;
+    //                 info!("Discover peripheral {:?} services...", &local_name);
+    //                 for service in peripheral.services() {
+    //                     if service.uuid == aranet_service_uuid {
+    //                         info!(
+    //                             "Service UUID {}, primary: {}",
+    //                             service.uuid, service.primary
+    //                         );
+    //                         for characteristic in service.characteristics {
+    //                             if characteristic.uuid == aranet_co2_measurement_characteristic_uuid
+    //                                 && characteristic.properties.contains(CharPropFlags::READ) {
+    //                                 let response = peripheral.read(&characteristic).await.expect("failed read");
+    //                                 debug!("response: {:?}", response);
+    //                                 let sample = sample::Sample::try_from(&response)?;
+    //                                 info!("{}: ARANET_CO2_MEASUREMENT_CHARACTERISTIC_UUID: {:?}", &local_name, sample);
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //                 info!("Disconnecting from peripheral {:?}...", &local_name);
+    //                 peripheral
+    //                     .disconnect()
+    //                     .await
+    //                     .expect("Error disconnecting from BLE peripheral");
+    //             }
+    //         }
+    //     }
         
-    }
+    // }
 
     Ok(())
 }
